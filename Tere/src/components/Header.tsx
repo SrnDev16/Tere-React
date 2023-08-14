5e1fd18334b46683a09761cd4f421b03f931641b
oat47110 <33687611+SrnDev16@users.noreply.github.com>
2023-08-14 10:14:39 +0700

    change logo
    Chang logo and size

--- Tere/src/components/Header.tsx
+++ Tere/src/components/Header.tsx
@@ -1,32 +1,43 @@
-import { useState } from "react"
-import Logo from "../assets/images/logo.svg"
-import { FiX , FiMenu } from "react-icons/fi";
-import './Header.css'
+import { useState } from "react";
+import Logo from "../assets/images/logo.svg";
+import { FiX, FiMenu } from "react-icons/fi";
+import "./Header.css";
 
 const Header = () => {
-    const [click ,setClick] = useState<boolean>(false);
-    const handleClick = () => setClick(!click);
+  const [click, setClick] = useState<boolean>(false);
+  const handleClick = () => setClick(!click);
 
   return (
-<div className="header">
-    <div className="container">
+    <div className="header">
+      <div className="container">
         <div className="header-con">
-            <div className="logo-con">
-                <img src={Logo} alt="logo"/>
-            </div>
-            <ul className={click ? "menu active":"menu"}>
-                <li><a href="$">Home</a></li>
-                <li><a href="$">How tere works</a></li>
-                <li><a href="$">Tere benefits</a></li>
-                <li><a href="$">Help Center</a></li>
-            </ul>
-            <div className="mobile-icon" onClick={handleClick}>
-                {click ? <FiX/> : <FiMenu/>}
-            </div>
+          <div className="logo-con">
+            <a href="#">
+              {" "}
+              <img src={Logo} alt="logo" />
+            </a>
+          </div>
+          <ul className={click ? "menu active" : "menu"}>
+            <li>
+              <a href="$">Home</a>
+            </li>
+            <li>
+              <a href="$">How tere works</a>
+            </li>
+            <li>
+              <a href="$">Tere benefits</a>
+            </li>
+            <li>
+              <a href="$">Help Center</a>
+            </li>
+          </ul>
+          <div className="mobile-icon" onClick={handleClick}>
+            {click ? <FiX className="icon"/> : <FiMenu className="icon"/>}
+          </div>
         </div>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
