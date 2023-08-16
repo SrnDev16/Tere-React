11078620de2a71c0909b7f5ffe02682fd9d96c6b
oat47110 <33687611+SrnDev16@users.noreply.github.com>
2023-08-16 11:20:19 +0700

    update section content 1
    fix content 1 and responsive

--- Tere/src/App.tsx
+++ Tere/src/App.tsx
@@ -1,16 +1,17 @@
-import Banner from "./components/Banner"
-import Content1 from "./components/Content1"
-import Header from "./components/Header"
+import Banner from "./components/Banner";
+import Benefits from "./components/Benefits";
+import Content1 from "./components/Content1";
+import Header from "./components/Header";
 
 function App() {
-
   return (
     <>
-    <Header/> 
-    <Banner/>
-    <Content1/>
+      <Header />
+      <Banner />
+      <Content1 />
+      <Benefits/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Tere/src/components/BenefitSection.tsx
@@ -0,0 +1,11 @@
+const BenefitSection = () => {
+  return <div className="container" style={Container}>
+    
+  </div>;
+};
+const Container = {
+  width: "1060px",
+  height: "304px",
+  borderRadius: "5px",
+};
+export default BenefitSection;
